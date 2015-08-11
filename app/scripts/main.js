7bdf3db92c85e33372092295e644c9724fa06e0d
sidhartha43 <sidhartha43@gmail.com>
2015-08-10 19:52:05 -0500

    Demo Ready1
    Menu animations on scrolls
    Added basic footer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,13 @@
 // jshint devel:true
 $(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 5) {
+            $('.navbar-purpletux').css({ "background-color": '#8d189f', 'padding-top': '3px', 'opacity': '0.94' });
+        }
+        if ($(window).scrollTop() < 5) {
+            $('.navbar-purpletux').css({ "background-color": 'transparent', 'padding-top': '40px', 'opacity': '1' });
+        }
+    });
+
     
 });
